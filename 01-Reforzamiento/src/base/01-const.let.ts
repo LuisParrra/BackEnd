b99cbf01dc673043399dd1d13bce8233314b2ef0
Luisparrra <lparrad77@gmail.com>
2025-08-26 09:18:26 -0600

    asdw

---
+++ 01-Reforzamiento/src/base/01-const.let.ts
@@ -0,0 +1,5 @@
+const firsname = "luis";
+const lastname = "parra";
+let secondname = "carlos";
+console.log("nombre "+firsname);
+console.log("apellido "+lastname);
--- 01-Reforzamiento/src/main.ts
+++ 01-Reforzamiento/src/main.ts
@@ -1,11 +1,12 @@
 import './style.css'
-
+import "./base/01-const.let"
 
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Hello mundo!</h1>
+    
   </div>
 `
 
