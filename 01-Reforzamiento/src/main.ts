3362a0c9fbf0c699c8d30bc3b13dc11f1247abae
Luisparrra <lparrad77@gmail.com>
2025-09-01 11:20:01 -0600

    objetos literales

--- 01-Reforzamiento/src/main.ts
+++ 01-Reforzamiento/src/main.ts
@@ -1,7 +1,10 @@
 import './style.css'
 //import "./base/01-const.let"
 import "./base/02-templateString"
-
+import "./base/03-Objetos-literales"
+import "./base/04-interfaces"
+import "./base/05-arays"
+import "./base/06-funciones"
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
